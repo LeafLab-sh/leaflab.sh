daf6b7053979cebab954065625ef6f515814891d
Lea Fairbanks <leafbot@proton.me>
2026-03-14 22:54:14 -0600

    chore: remove log message

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,13 +1,7 @@
 import { defineMiddleware } from "astro:middleware";
 import { logger } from "../utils/logger";
-import { LogEvent } from "../utils/logger";
 
 export const loggerMiddleware = defineMiddleware(async (context, next) => {
   context.locals.logger = logger;
-  const url = new URL(context.request.url);
-  logger.info({
-    event: LogEvent.REQUEST,
-    message: "User accessed " + url.pathname,
-  });
   return next();
 });
